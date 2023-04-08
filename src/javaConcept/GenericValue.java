61741bccb341a01f802c17312dd8ab925f4c535e
ali-khosravani <ali.p3soft@gmail.com>
2023-04-08 04:39:35 +0330

    Declare variable

---
+++ src/javaConcept/GenericValue.java
@@ -0,0 +1,23 @@
+package javaConcept;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class GenericValue {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(8);
+        list.add(1);
+        list.add(6);
+        list.add(3);
+        list.add(4);
+        list.add(9);
+        list.add(5);
+        Collections.sort(list);
+        System.out.println(list);
+        Collections.sort(list , Collections.reverseOrder());
+        System.out.println(list);
+    }
+
+}
---
+++ src/javaConcept/Variable.java
@@ -0,0 +1,24 @@
+package javaConcept;
+
+/**
+ * JavaDoc
+ * Author: Ali Khosravani
+ * Date: 2023-04-08
+ * Desc: Declare variable
+ */
+public class Variable {
+    //single comment
+    /*
+    multi line comment
+     */
+    public static void main(String[] args) {
+        //final is read only variable and don't overwrite existing value
+        String str = "Ali Khosravani";
+        int number = 10;
+        final int numberFinal = 25;
+        float numberFlout = 10.25f;
+        char myChar = 'a';
+        boolean bool = true;
+        System.out.println(bool);
+    }
+}
